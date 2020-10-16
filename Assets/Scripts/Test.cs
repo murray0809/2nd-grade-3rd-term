cc6baacc21a0587bfb542664a95cd3a15724956b
murray0809 <tomo19940809@gmail.com>
2020-10-16 14:22:42 +0900

    ワイヤーギミックを実装した

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] GameObject test;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            test.SetActive(true);
+        }
+        else
+        {
+            test.SetActive(false);
+        }
+    }
+}
