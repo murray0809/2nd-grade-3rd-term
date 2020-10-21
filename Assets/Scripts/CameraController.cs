0dd6cdab97f036ab59cba60094868358db48569b
murray0809 <tomo19940809@gmail.com>
2020-10-21 14:18:33 +0900

    ワイヤーの実装

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,14 +7,55 @@ public class CameraController : MonoBehaviour
     [SerializeField] GameObject mainCamera;              //メインカメラ格納用
     [SerializeField] GameObject playerObject;            //回転の中心となるプレイヤー格納用
     [SerializeField] float rotateSpeed = 2.0f;            //回転の速さ
+
+    public Transform cam;
+    private RaycastHit hit;
+    private Rigidbody rb;
+    private bool attached = false;
+    public float momentum;
+    public float speed;
+    public float step;
+
+    public LayerMask layerMask;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         rotateCamera();
+
+
+        if (Input.GetButton("Fire1"))
+        {
+            if (Physics.Raycast(cam.position, cam.forward, out hit))
+            {
+                Debug.DrawRay(cam.position, cam.forward * 100, Color.yellow);
+                attached = true;
+                rb.isKinematic = true;
+            }
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            attached = false;
+            rb.isKinematic = false;
+            rb.velocity = transform.forward * momentum;
+        }
+
+        if (attached)
+        {
+            momentum += Time.deltaTime * speed;
+            step = momentum * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, hit.point, step);
+        }
+
+        if (!attached && momentum >= 0)
+        {
+            momentum -= Time.deltaTime * 5;
+            step = 0;
+        }
     }
 
     private void rotateCamera()
--- Assets/Scripts/RaycastController.cs
+++ Assets/Scripts/RaycastController.cs
@@ -6,16 +6,24 @@ using UnityEngine.UI;
 public class RaycastController : MonoBehaviour
 {
     [SerializeField] Camera cam = default;
-    Vector3 pos = new Vector3(200, 400, 0);
+    Vector3 pos = new Vector3(200, 200, 0);
 
-    public LayerMask mask;
+    //public LayerMask mask;
 
     [SerializeField] Image aim = default; // 照準のImageをインスペクターから設定
 
     public GameObject test;
+
+    private SpringJoint joint;
+
+    [SerializeField] GameObject player;
+
+    Rigidbody m_rb;
+
+    RaycastHit hit;
     void Start()
     {
-        
+        m_rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -23,33 +31,72 @@ public class RaycastController : MonoBehaviour
         Ray ray = cam.ScreenPointToRay(pos);
         Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
 
-        RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        if (Physics.Raycast(ray, out hit,100f))
         {
-            //hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
-            
-            string hitTag = hit.collider.tag; // Rayがhitしたオブジェクトのタグ名を取得
-           
-            if ((hitTag.Equals("Target")))
+            Debug.Log(hit.point);
+            aim.color = new Color(1.0f, 0.0f, 0.0f, 1.0f); //照準を赤に変える
+            if (Input.GetMouseButtonDown(0))
             {
-                aim.color = new Color(1.0f, 0.0f, 0.0f, 1.0f); //照準を赤に変える
-                //Debug.Log(hit.collider.gameObject.transform.position);
-
-                test = hit.collider.gameObject;
+                Joint();
             }
-            else
+            else if (Input.GetMouseButtonUp(0))
             {
-                aim.color = new Color(0.0f, 1.0f, 1.0f, 1.0f); //照準を青に変える
-
-                test = null;
+                DestroyJoint();
             }
         }
         else
         {
-            aim.color = new Color(0.0f, 1.0f, 1.0f, 1.0f); // Rayがヒットしていない場合は水色に
-
-            test = null;
+            aim.color = new Color(0.0f, 1.0f, 1.0f, 1.0f); //照準を青に変える
         }
+
+
+
+
+        //if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        //{
+        //    string hitTag = hit.collider.tag; // Rayがhitしたオブジェクトのタグ名を取得
+
+        //    if ((hitTag.Equals("Target")))
+        //    {
+        //        aim.color = new Color(1.0f, 0.0f, 0.0f, 1.0f); //照準を赤に変える
+        //        Debug.Log(hit.collider.gameObject.transform.position);
+
+        //        test = hit.collider.gameObject;
+        //    }
+        //    else
+        //    {
+        //        aim.color = new Color(0.0f, 1.0f, 1.0f, 1.0f); //照準を青に変える
+
+        //        test = null;
+        //    }
+        //}
+        //else
+        //{
+        //    aim.color = new Color(0.0f, 1.0f, 1.0f, 1.0f); // Rayがヒットしていない場合は水色に
+
+        //    test = null;
+        //}
+    }
+
+    void Joint()
+    {
+        joint = player.gameObject.AddComponent<SpringJoint>();
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedAnchor = hit.point;
+
+        float distanceFromPoint = Vector3.Distance(player.transform.position, hit.point);
+
+        joint.maxDistance = 0;
+        joint.minDistance = 0;
+
+        joint.spring = 4.5f;
+        joint.damper = 7f;
+        joint.massScale = 4.5f;
+    }
+
+    void DestroyJoint()
+    {
+        Destroy(joint);
     }
 }
