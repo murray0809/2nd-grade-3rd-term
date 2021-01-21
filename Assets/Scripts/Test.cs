1103b718df6eff72e8d1be713bb816f2fe9c887a
吉川智浩 <>
2021-01-21 22:19:46 +0900

    テスト

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    bool test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(test);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        test = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        test = false;
+    }
+}
