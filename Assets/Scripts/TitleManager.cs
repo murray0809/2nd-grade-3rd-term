5d3e0bc113916cb3c668962e77ff84dd711a8e20
吉川智浩 <>
2021-04-15 15:22:29 +0900

    タイトル画面を修正した

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// タイトル管理のスクリプト
+/// </summary>
+public class TitleManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("StageSelect");
+        }
+    }
+}
