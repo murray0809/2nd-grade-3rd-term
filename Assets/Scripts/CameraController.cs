9f1f7066fb6d638f3c78ff1ca74a3b76ce3389d2
吉川智浩 <tomo19940809@gmail.com>
2021-05-17 23:49:15 +0900

    カメラを追加した

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// キャラクターを追いかけるカメラのスクリプト
+/// </summary>
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] GameObject m_player;
+
+    PlayerController playerController;
+
+    [SerializeField] float m_addY;
+
+    [SerializeField] float m_addZ;
+
+    void Start()
+    {
+        m_player = GameObject.FindGameObjectWithTag("Player");
+        playerController = m_player.GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        Transform myTransform = this.transform;
+
+        Vector3 myPos = myTransform.transform.position;
+
+        float xPos = m_player.transform.position.x;
+        myPos.x = xPos;
+
+        float yPos;
+        if (playerController.Jumping)
+        {
+            yPos = m_player.transform.position.y * 100;
+            yPos = Mathf.Floor(yPos);
+            yPos = yPos / 100;
+            myPos.y = yPos + m_addY;
+        }
+        else
+        {
+            yPos = m_player.transform.position.y * 10;
+            yPos = Mathf.Floor(yPos);
+            yPos = yPos / 10;
+            myPos.y = yPos + m_addY;
+        }
+        
+        float zPos = m_player.transform.position.z;
+        myPos.z = zPos + m_addZ;
+
+        myTransform.transform.position = myPos;
+    }
+}
