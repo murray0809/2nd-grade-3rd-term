d33f1696dd0111170df1786341d0d9b71c781bc9
吉川智浩 <tomo19940809@gmail.com>
2021-05-30 22:32:40 +0900

    MoveObjectにコメントを追加した

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 動かせるオブジェクトのスクリプト
+/// </summary>
 public class MoveObject : MonoBehaviour
 {
     public PlayerController playerController;
 
+    /// <summary>
+    /// プレイヤーが触れている間表示するアイコン
+    /// </summary>
     [SerializeField] GameObject m_catchig;
 
     public void Start()
@@ -23,6 +29,10 @@ public class MoveObject : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// プレイヤーが動いていない状態で触れたら動かせるようにする
+    /// </summary>
+    /// <param name="collision"></param>
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player") && !playerController.MovingObject)
@@ -31,6 +41,10 @@ public class MoveObject : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// プレイヤーが離れたら動かないようにする
+    /// </summary>
+    /// <param name="collision"></param>
     public void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player") && !playerController.Catching)
@@ -41,6 +55,10 @@ public class MoveObject : MonoBehaviour
         m_catchig.SetActive(false);
     }
 
+    /// <summary>
+    /// プレイヤーが触れている間、アイコンを表示する
+    /// </summary>
+    /// <param name="collision"></param>
     public void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
